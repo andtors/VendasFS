4b5c021303272f85327bdb5698c42eb623643705
André Torres <150715327+andtors@users.noreply.github.com>
2024-08-21 15:35:33 -0300

    SC-3-19

--- vendasfront/src/app/components/produtos/cadastros/CadastroProdutos.tsx
+++ vendasfront/src/app/components/produtos/cadastros/CadastroProdutos.tsx
@@ -1,18 +1,87 @@
-import React from 'react'
-import {Layout} from '@/app/components'
+'use client'
+
+import { Layout } from '@/app/components'
+import { useState } from 'react'
 
 type Props = {}
 
 export const CadastroProdutos = (props: Props) => {
+
+  const [sku, setSku] = useState("")
+  const [preco, setPreco] = useState("")
+  const [nome, setNome] = useState("")
+  const [descricao, setDescricao] = useState("")
+
+  function submit() {
+    const produto = {
+      sku, preco, nome, descricao
+    }
+    console.log(produto)
+  }
+
   return (
     <Layout titulo='Cadastro de produtos' >
-      <div>
-        <p>Teste</p>
+      <div className="columns">
+        <div className='field is-half column'>
+          <label className='label' htmlFor='inputSku'>SKU: *</label>
+          <div className="control">
+            <input className='input'
+              id="inputSku"
+              value={sku}
+              onChange={(e) => setSku(e.target.value)}
+              placeholder='Digite o SKU do produto' />
+          </div>
+        </div>
+        <div className='field is-half column'>
+          <label className='label' htmlFor='inputPreco'>Preço: *</label>
+          <div className="control">
+            <input className='input'
+              id="inputPreco"
+              value={preco}
+              onChange={(e) => setPreco(e.target.value)}
+              placeholder='Digite o preço do produto' />
+          </div>
+        </div>
+      </div>
+      <div className="columns">
+        <div className='field is-full column'>
+          <label className='label' htmlFor='inputNome'>Nome: *</label>
+          <div className="control">
+            <input className='input'
+              id="inputNome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              placeholder='Digite o nome do produto' />
+          </div>
+        </div>
+      </div>
+      <div className="columns">
+        <div className='field is-full column'>
+          <label className='label' htmlFor='inputDesc'>Descrição: *</label>
+          <div className="control">
+            <textarea className='textarea'
+              id="inputDesc"
+              value={descricao}
+              onChange={(e) => setDescricao(e.target.value)}
+              placeholder="Digite a descrição do produto">
+            </textarea>
+          </div>
+        </div>
+      </div>
+      <div className='field is-grouped'>
+        <div className='control'>
+          <button className='button is-link' onClick={submit}>
+            Salvar
+          </button>
+        </div>
+        <div className='control'>
+          <button className='button is-link is-light'>
+            Voltar
+          </button>
+        </div>
       </div>
     </Layout>
-     
-
   )
 }
 
- 
+
