90ea16dc8bd431945ac17851cb5b202d6cd67741
André Torres <150715327+andtors@users.noreply.github.com>
2024-09-30 15:40:47 -0300

    SC-5-85

--- vendasfront/src/app/components/vendas/VendasForm.tsx
+++ vendasfront/src/app/components/vendas/VendasForm.tsx
@@ -5,7 +5,8 @@ import { useFormik } from "formik"
 import { AutoComplete, AutoCompleteChangeParams, AutoCompleteCompleteMethodParams } from 'primereact/autocomplete'
 import { useState } from "react"
 import { useClienteService } from "@/app/api/services"
-import {Button} from 'primereact/button'
+import { Button } from 'primereact/button'
+import { InputText } from 'primereact/inputtext'
 
 interface VendaFormProps {
     onSubmit: (venda: IVendas) => void
@@ -23,6 +24,7 @@ export const VendasForm: React.FC<VendaFormProps> = ({
 }) => {
 
     const service = useClienteService()
+    const [codigoProduto, setCodigoProduto] = useState<string>('')
     const [listaClientes, setListaClientes] = useState<IPage<ICliente>>({
         content: [],
         first: 0,
@@ -39,13 +41,17 @@ export const VendasForm: React.FC<VendaFormProps> = ({
     const handleClienteAutocomplete = (e: AutoCompleteCompleteMethodParams) => {
         const nome = e.query
         service
-        .find(nome, '', 0, 20)
-        .then(clientes => setListaClientes(clientes))
+            .find(nome, '', 0, 20)
+            .then(clientes => setListaClientes(clientes))
     }
 
     const handleClienteChange = (e: AutoCompleteChangeParams) => {
         const clienteSelecionado: ICliente = e.value
         formik.setFieldValue("cliente", clienteSelecionado)
+    }
+
+    const handleCodigoProdutoSelect = (e) => {
+        console.log(codigoProduto)
     }
 
     return (
@@ -58,12 +64,38 @@ export const VendasForm: React.FC<VendaFormProps> = ({
                         completeMethod={handleClienteAutocomplete}
                         value={formik.values.cliente}
                         id="cliente"
-                        name="cliente" 
+                        name="cliente"
                         field="nome"
                         onChange={handleClienteChange}
-                        />
+                    />
                 </div>
-                <Button type="submit" label="Finalizar"/>
+
+                <div className="p-grid">
+                    <div className="p-col-2">
+                        <span className="p-float-label">
+                            <InputText
+                                onBlur={handleCodigoProdutoSelect}
+                                value={codigoProduto}
+                                id="codigoProduto"
+                                onChange={e => setCodigoProduto(e.target.value)} />
+                            <label htmlFor="codigoProduto">Código</label>
+                        </span>
+                    </div>
+                    <div className="p-col-6">
+                        <AutoComplete />
+                    </div>
+                    <div className="p-col-2">
+                    <span className="p-float-label">
+                        <InputText id="qtdProduto" />
+                            <label htmlFor="qtdProduto">QTD</label>
+                    </span>
+                    </div>
+                    <div className="p-col-2">
+                       <Button  label="Adicionar"/>
+                    </div>
+                </div>
+
+                <Button type="submit" label="Finalizar" />
             </div>
         </form>
     )
